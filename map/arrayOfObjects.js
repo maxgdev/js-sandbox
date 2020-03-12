486c38a5e9887f1fe39cbd95974a1900ed1ef28f
maxgdev <max@apphilia.net>
2020-03-12 12:08:44 +0000

    Updates to arrayOfObjects.js

--- map/arrayOfObjects.js
+++ map/arrayOfObjects.js
@@ -1,19 +1,17 @@
-// Iteracting over an array of objects
+// Iterating over an array of objects
 // A usefull contruct in react componets to display lists of DataTransferItemList
 
 const values = [
     { label: 'First', checked: false },
     { label: 'Second', checked: false },
-    { label: 'Third', checked: false }
+    { label: 'Third', checked: false },
+    { label: 'Fourth', checked: true }
   ];
 
 const testfn = (values ) => (
-
     values.map((value, index) => {
         console.log("Index: "+ index, " Checked: "+value.checked, " Label: "+value.label)
-    })
-      
+        }) 
     );
-
 
 testfn(values)
